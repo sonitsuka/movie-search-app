422d59adc606953b83a7d1937223c9a0a723938d
sonitsuka <onitsukashiho@gmail.com>
2022-05-19 15:34:30 +0200

    Add css SearchMovie

--- src/SearchMovie.js
+++ src/SearchMovie.js
@@ -5,7 +5,7 @@ export default function SearchMovie(){
     <div>
         <form className='form'>
             <label className='label' htmlFor='query'>movie name</label>
-            <input type='text' name='query' placeholder='e.g.  '></input>
+            <input className='input' type='text' name='query' placeholder='e.g. Leon'></input>
             <button className='button' type='submit'>Search</button>
         </form>
     </div>
