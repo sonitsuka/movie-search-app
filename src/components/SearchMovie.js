6a1650e1f1181d3cffb2ab6b3b05310c0e8def31
sonitsuka <onitsukashiho@gmail.com>
2022-05-20 18:57:08 +0200

    Changed  placeholder text

--- src/components/SearchMovie.js
+++ src/components/SearchMovie.js
@@ -12,24 +12,25 @@ export default function SearchMovie(){
     //console.log('submmiting');
   
     
-  const SEARCH_API ="https://api.themoviedb.org/3/search/movie?&api_key=bc6c6d479cb8166b0fa55facea70e9b2&query=";
+    const SEARCH_API ="https://api.themoviedb.org/3/search/movie?&api_key=bc6c6d479cb8166b0fa55facea70e9b2&query=";
 
-  try {
-    const res = await fetch(SEARCH_API + query);
-    const data = await res.json();
-    console.log(data.results)
-    setMovies(data.results)
-  }catch(err){
-    console.log(err)
+    try {
+      const res = await fetch(SEARCH_API + query);
+      const data = await res.json();
+      console.log(data.results)
+      setMovies(data.results)
+    }catch(err){
+      console.log(err)
+    }
+
   }
-
-}
   return (
     <> 
         <form className='form' onSubmit={searchMovies}>
             <label className='label' htmlFor='query'>title</label>
-            <input className='input' type='text' name='query' 
-                  placeholder='e.g. Leon' 
+            <input className='input' type='search' 
+                  name='query' 
+                  placeholder='Search...' 
                   value={query}
                   onChange ={(e) => setQuery(e.target.value)}>
             </input>
